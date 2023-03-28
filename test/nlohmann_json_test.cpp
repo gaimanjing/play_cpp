1fe7740676b8d9630a54d6bfa2790516db0028e4
Guanhua Hou <gaimanjing@gmail.com>
2023-03-28 22:34:31 +0800

    _

--- test/nlohmann_json_test.cpp
+++ test/nlohmann_json_test.cpp
@@ -5,6 +5,32 @@
 using nlohmann::json;
 
 TEST(JsonTest, Initializer_lists_construct) {
-  json j{true};
+  json j{true, 2};
   EXPECT_TRUE(j.is_array());
+}
+
+TEST(JsonTest, construct) {
+  json j(true);
+  EXPECT_TRUE(j.is_boolean());
+}
+
+// https://json.nlohmann.me/features/parsing/parse_exceptions/
+TEST(JsonTest, parse_exception) {
+  auto my_input = "{";
+  try {
+    auto j = json::parse(my_input);
+  } catch (json::parse_error &ex) {
+    std::cerr << "parse error at byte " << ex.byte << std::endl;
+    GTEST_SUCCEED();
+  }
+}
+
+TEST(JsonTest, parse_no_exception) {
+  auto my_input = "{";
+  json j = json::parse(my_input, nullptr, false);
+  if (j.is_discarded()) {
+    std::cerr << "parse error" << std::endl;
+  }
+
+  GTEST_SUCCEED();
 }
